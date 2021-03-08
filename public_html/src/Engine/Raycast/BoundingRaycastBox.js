4acf77a7a2f7c804eb0e0004380965a3ac50c327
Gabriel Oliver <olivergabe@hotmail.com>
2021-03-07 21:18:13 -0800

    trying to prevent "collision" but weird bug

--- public_html/src/Engine/Raycast/BoundingRaycastBox.js
+++ public_html/src/Engine/Raycast/BoundingRaycastBox.js
@@ -8,6 +8,7 @@ function BoundingRaycastBox(centerPos, w, h) {
     this.kSpriteSheetTexture = "assets/minion_sprite.png";
     this.boundRend = new Renderable(this.kSpriteSheetTexture);
     let xform = this.boundRend.getXform();
+    this.myXform = xform;
     xform.setSize(7.5, 7.5);
     xform.setPosition(centerPos[0], centerPos[1]);
     this.boundRend.setColor([1, 1, 1, 1]);
@@ -49,18 +50,54 @@ BoundingRaycastBox.prototype.checkIntersection = function (raycast) {
     let line = raycast.getLine();
     let cornerTracker = [];
 
-    for (let i = 0; i < this.corners.length; i++) {
-        if (line[0] * this.corners[i][0] + line[1] > this.corners[i][1]) {
-            cornerTracker.push(0);
-        } else if (line[0] * this.corners[i][0] + line[1] < this.corners[i][1]) {
-            cornerTracker.push(1);
-        } else {
-            return true;
-        }
+    let rayEndPoint = raycast.getEndPoint();
+    let rayStartPoint = raycast.getStartPoint();
+    let myXPos = this.myXform.getPosition()[0];
+    let myYPos = this.myXform.getPosition()[1];
+    //console.log(rayEndPoint[0]);
+    if ((rayEndPoint[0] >= (myXPos - this.halfWidth)
+        && rayEndPoint[0] <= (myXPos + this.halfWidth))
+        && (rayEndPoint[1] >= (myYPos - this.halfHeight)
+        && rayEndPoint[1] <= (myYPos + this.halfWidth))) {
+        return true;
     }
-    for (let i = 0; i < cornerTracker.length - 1; i++) {
-        if (cornerTracker[i] !== cornerTracker[i + 1]) {
-            return true;
+
+    // Gabe: This isnt working for some reason???????????????????????????????????????????????????????????
+    let startIsLeft = rayStartPoint[0] < (myXPos - this.halfWidth);
+    let startIsRight = rayStartPoint[0] > myXPos + this.halfWidth;
+    let startIsAbove = rayStartPoint[1] > myYPos + this.halfHeight;
+    let startIsBelow = rayStartPoint[1] < myYPos - this.halfHeight;
+    let endIsLeft = rayEndPoint[0] < myXPos - this.halfWidth;
+    let endIsRight = rayEndPoint[0] > myXPos + this.halfWidth;
+    let endIsAbove = rayEndPoint[1] > myXPos + this.halfHeight;
+    let endIsBelow = rayEndPoint[1] < myXPos - this.halfHeight;
+    console.log("startLeft: " + startIsLeft);
+    console.log("rayStartPoint[0]: " + rayStartPoint[0]);
+    console.log("myXPos - this.halfwidth: " + (myXPos - this.halfWidth)); // cause of this???????????????????
+
+    if (startIsLeft && endIsLeft) {
+        return false;
+    } else if (startIsRight && endIsRight) {
+        return false;
+    } else if (startIsAbove && endIsAbove) {
+        return false;
+    } else if (startIsBelow && endIsBelow) {
+        return false;
+    } else {
+
+        for (let i = 0; i < this.corners.length; i++) {
+            if (line[0] * this.corners[i][0] + line[1] > this.corners[i][1]) {
+                cornerTracker.push(0);
+            } else if (line[0] * this.corners[i][0] + line[1] < this.corners[i][1]) {
+                cornerTracker.push(1);
+            } else {
+                return true;
+            }
+        }
+        for (let i = 0; i < cornerTracker.length - 1; i++) {
+            if (cornerTracker[i] !== cornerTracker[i + 1]) {
+                return true;
+            }
         }
     }
     return false;
